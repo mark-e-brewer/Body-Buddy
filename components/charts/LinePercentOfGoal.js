b6feb7104d46878c2a35a2310ffa87ee1cb92e6b
Mark Brewer <markbrewer369@gmail.com>
2023-06-02 17:40:26 -0500

    fixed week calculations problem

---
+++ components/charts/LinePercentOfGoal.js
@@ -0,0 +1,12 @@
+// const percentageComplete = {};
+// for (const muscle in weekObj) {
+//   if (muscle.includes('Goal')) {
+//     const total = weekObj[muscle.replace('Goal', 'Total')];
+//     const goal = weekObj[muscle];
+
+//     // Calculate the percentage based on the minimum of total and goal
+//     const percent = Math.min((total / goal) * 100, 100);
+//     // Store the percentage for the muscle group
+//     percentageComplete[muscle.replace('Goal', '')] = percent;
+//   }
+// }
