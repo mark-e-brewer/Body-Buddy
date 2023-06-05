5807a401bd383c4c72f5e5f2792a537f7f7b92cc
Mark Brewer <markbrewer369@gmail.com>
2023-06-04 22:42:19 -0500

    line graph percent fix to last4 week and num vs goal setup

--- components/charts/LinePercentOfGoal.js
+++ components/charts/LinePercentOfGoal.js
@@ -1,15 +1,20 @@
-/* eslint-disable no-unused-vars */
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRightLong } from '@fortawesome/free-solid-svg-icons';
 import {} from 'charts.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import StatOptionsMuscleGroups from '../StatFilterButtons/StatOptionsMuscleGroups';
-import StatOptionToggleGoal from '../StatFilterButtons/StatOptionToggleGoal';
-import StatOptionNumberPercent from '../StatFilterButtons/StatOptionNumberPercent';
 
-export default function LinePercentOfGoal({ weekArray, weekArray2 }) {
+export default function LinePercentOfGoal({ weekArray }) {
+  const [prevMonth, setPrevMonth] = useState([]);
   const [muscleGroup, setMuscleGroup] = useState('All');
+  useEffect(() => {
+    const highestFourWeeks = weekArray?.slice(-4);
+    setPrevMonth(highestFourWeeks);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [weekArray]);
+  console.warn('hi');
+
   let muscleSelected = null;
   let week1percent = 0;
   let week2percent = 0;
@@ -45,55 +50,72 @@ export default function LinePercentOfGoal({ weekArray, weekArray2 }) {
   }
 
   if (muscleSelected === null) {
-    let totalGoalWeek1 = 0;
-    let totalCompletedWeek1 = 0;
-    let totalGoalWeek2 = 0;
-    let totalCompletedWeek2 = 0;
-    let totalGoalWeek3 = 0;
-    let totalCompletedWeek3 = 0;
-    let totalGoalWeek4 = 0;
-    let totalCompletedWeek4 = 0;
-    weekArray.forEach((weekObj) => {
-      let totalGoal = 0;
-      let totalCompleted = 0;
-      Object.keys(weekObj).forEach((key) => {
-        if (key.endsWith('Goal')) {
-          const totalKey = key.replace('Goal', 'Total');
-          const goal = weekObj[key];
-          const total = weekObj[totalKey];
-          const completed = Math.min(total, goal);
-          totalGoal += goal;
-          totalCompleted += completed;
-        }
-      });
-      // eslint-disable-next-line default-case
-      switch (weekObj.weekNum) {
-        case 1:
-          totalGoalWeek1 = totalGoal;
-          totalCompletedWeek1 = totalCompleted;
-          break;
-        case 2:
-          totalGoalWeek2 = totalGoal;
-          totalCompletedWeek2 = totalCompleted;
-          break;
-        case 3:
-          totalGoalWeek3 = totalGoal;
-          totalCompletedWeek3 = totalCompleted;
-          break;
-        case 4:
-          totalGoalWeek4 = totalGoal;
-          totalCompletedWeek4 = totalCompleted;
-          break;
+    const weekObj1 = prevMonth[0];
+    const weekObj2 = prevMonth[1];
+    const weekObj3 = prevMonth[2];
+    const weekObj4 = prevMonth[3];
+
+    let totalGoal1 = 0;
+    let totalCompleted1 = 0;
+    let totalGoal2 = 0;
+    let totalCompleted2 = 0;
+    let totalGoal3 = 0;
+    let totalCompleted3 = 0;
+    let totalGoal4 = 0;
+    let totalCompleted4 = 0;
+
+    Object?.keys(weekObj1 || {}).forEach((key) => {
+      if (key?.endsWith('Goal')) {
+        const totalKey = key.replace('Goal', 'Total');
+        const goal = weekObj1[key];
+        const total = weekObj1[totalKey];
+        const completed = Math.min(total, goal);
+        totalGoal1 += goal;
+        totalCompleted1 += completed;
       }
     });
-    week1percent = Math.ceil((totalCompletedWeek1 / totalGoalWeek1) * 100);
-    week2percent = Math.ceil((totalCompletedWeek2 / totalGoalWeek2) * 100);
-    week3percent = Math.ceil((totalCompletedWeek3 / totalGoalWeek3) * 100);
-    week4percent = Math.ceil((totalCompletedWeek4 / totalGoalWeek4) * 100);
+
+    Object?.keys(weekObj2 || {}).forEach((key) => {
+      if (key?.endsWith('Goal')) {
+        const totalKey = key.replace('Goal', 'Total');
+        const goal = weekObj2[key];
+        const total = weekObj2[totalKey];
+        const completed = Math.min(total, goal);
+        totalGoal2 += goal;
+        totalCompleted2 += completed;
+      }
+    });
+
+    Object?.keys(weekObj3 || {}).forEach((key) => {
+      if (key?.endsWith('Goal')) {
+        const totalKey = key.replace('Goal', 'Total');
+        const goal = weekObj3[key];
+        const total = weekObj3[totalKey];
+        const completed = Math.min(total, goal);
+        totalGoal3 += goal;
+        totalCompleted3 += completed;
+      }
+    });
+
+    Object?.keys(weekObj4 || {}).forEach((key) => {
+      if (key?.endsWith('Goal')) {
+        const totalKey = key.replace('Goal', 'Total');
+        const goal = weekObj4[key];
+        const total = weekObj4[totalKey];
+        const completed = Math.min(total, goal);
+        totalGoal4 += goal;
+        totalCompleted4 += completed;
+      }
+    });
+
+    week1percent = Math.ceil((totalCompleted1 / totalGoal1) * 100);
+    week2percent = Math.ceil((totalCompleted2 / totalGoal2) * 100);
+    week3percent = Math.ceil((totalCompleted3 / totalGoal3) * 100);
+    week4percent = Math.ceil((totalCompleted4 / totalGoal4) * 100);
   } else if (muscleSelected !== null) {
     const percentageComplete = {};
     for (let i = 0; i < 4; i++) {
-      const weekObj = weekArray2[i];
+      const weekObj = prevMonth[i];
       const weekPercentsComplete = {};
       // eslint-disable-next-line no-restricted-syntax
       for (const muscle in weekObj) {
@@ -106,10 +128,10 @@ export default function LinePercentOfGoal({ weekArray, weekArray2 }) {
       }
       percentageComplete[`week${i + 1}PercentsComplete`] = weekPercentsComplete;
     }
-    week1percent = Math.ceil(percentageComplete.week1PercentsComplete[muscleSelected]);
-    week2percent = Math.ceil(percentageComplete.week2PercentsComplete[muscleSelected]);
-    week3percent = Math.ceil(percentageComplete.week3PercentsComplete[muscleSelected]);
-    week4percent = Math.ceil(percentageComplete.week4PercentsComplete[muscleSelected]);
+    week1percent = Math.ceil(percentageComplete?.week1PercentsComplete[muscleSelected]);
+    week2percent = Math.ceil(percentageComplete?.week2PercentsComplete[muscleSelected]);
+    week3percent = Math.ceil(percentageComplete?.week3PercentsComplete[muscleSelected]);
+    week4percent = Math.ceil(percentageComplete?.week4PercentsComplete[muscleSelected]);
   }
 
   return (
@@ -119,18 +141,18 @@ export default function LinePercentOfGoal({ weekArray, weekArray2 }) {
           <h3 style={{ marginBottom: '12px' }} className="d-flex justify-content-center">Last Four Weeks Percentages</h3>
           <tbody>
             <tr>
-              <p className="line-percent-goal-datatext" style={{ position: 'absolute', top: `${96 - week1percent}%` }}>{week1percent}%</p>
-              <th className="line-percent-goal-label" scope="row">week {weekArray[1].weekNum}<FontAwesomeIcon icon={faRightLong} /></th>
+              <p className="line-percent-goal-datatext" style={{ position: 'absolute', top: `${96 - week1percent}%`, left: '-9.5%' }}>{week1percent}%</p>
+              <th className="line-percent-goal-label" scope="row">week {prevMonth[1]?.weekNum}<FontAwesomeIcon icon={faRightLong} /></th>
               <td className="line-percent-goal-data" style={{ '--start': `${(week1percent / 100) - 0.005}`, '--size': `${(week2percent / 100) - 0.005}` }}><span className="line-percent-goal-datatext">{week2percent}%</span></td>
               {/* <td style={{ '--start': '0.0', '--size': '0.0' }}><span className="line-percent-goal-datatext"> 20 </span></td> */}
             </tr>
             <tr>
-              <th className="line-percent-goal-label" scope="row">week {weekArray[2].weekNum}<FontAwesomeIcon icon={faRightLong} /></th>
+              <th className="line-percent-goal-label" scope="row">week {prevMonth[2]?.weekNum}<FontAwesomeIcon icon={faRightLong} /></th>
               <td className="line-percent-goal-data" style={{ '--start': `${(week2percent / 100) - 0.005}`, '--size': `${(week3percent / 100) - 0.005}` }}><span className="line-percent-goal-datatext">{week3percent}%</span></td>
               {/* <td style={{ '--start': '0.0', '--size': '0.5' }}><span className="line-percent-goal-datatext"> 50 </span></td> */}
             </tr>
             <tr>
-              <th className="line-percent-goal-label" scope="row">week {weekArray[3].weekNum}<FontAwesomeIcon icon={faRightLong} /></th>
+              <th className="line-percent-goal-label" scope="row">week {prevMonth[3]?.weekNum}<FontAwesomeIcon icon={faRightLong} /></th>
               <td className="line-percent-goal-data" style={{ '--start': `${(week3percent / 100) - 0.005}`, '--size': `${(week4percent / 100) - 0.005}` }}><span className="line-percent-goal-datatext">{week4percent}%</span></td>
               {/* <td style={{ '--start': '0.5', '--size': '0.3' }}><span className="line-percent-goal-datatext"> 30 </span></td> */}
             </tr>
@@ -146,18 +168,4 @@ export default function LinePercentOfGoal({ weekArray, weekArray2 }) {
 
 LinePercentOfGoal.propTypes = {
   weekArray: PropTypes.arrayOf(PropTypes.shape).isRequired,
-  weekArray2: PropTypes.arrayOf(PropTypes.shape).isRequired,
 };
-
-// const percentageComplete = {};
-// // eslint-disable-next-line no-restricted-syntax
-// for (const muscle in weekObj) {
-//   if (muscle.includes('Goal')) {
-//     const total = weekObj[muscle.replace('Goal', 'Total')];
-//     const goal = weekObj[muscle];
-//     // Calculate the percentage based on the minimum of total and goal
-//     const percent = Math.min((total / goal) * 100, 100);
-//     // Store the percentage for the muscle group
-//     percentageComplete[muscle.replace('Goal', '')] = percent;
-//   }
-// }
