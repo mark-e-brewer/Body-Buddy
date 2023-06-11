e17e3d4e145358baf4cc5980a84e632fe220fef6
Mark Brewer <markbrewer369@gmail.com>
2023-06-10 23:32:15 -0500

    workout gen setup

---
+++ API/exerciseData.js
@@ -0,0 +1,25 @@
+const bicepWorkArr = [
+  {
+    exercise: 'Dumbbell Bicep Curls',
+    muscleGroup: 'bicep',
+  },
+  {
+    exercise: 'Flat-Bar Bicep Curls',
+    muscleGroup: 'bicep',
+  },
+];
+const tricepWorkArr = [
+  {
+    exercise: 'Skullcrushers',
+    muscleGroup: 'tricep',
+  },
+  {
+    exercise: 'Overhead Dumbbell Triceps Extensions',
+    muscleGroup: 'tricep',
+  },
+];
+
+export {
+  bicepWorkArr,
+  tricepWorkArr,
+};
---
+++ components/forms/WorkoutGenerator.js
@@ -0,0 +1,31 @@
+// import React, { useEffect, useState } from 'react';
+// import PropTypes from 'prop-types';
+// import Form from 'react-bootstrap/Form';
+// import Button from 'react-bootstrap/Button';
+// import { bicepWorkArr, tricepWorkArr } from '../../API/exerciseData';
+// import { postExercise, patchExercise } from '../../API/apiData';
+
+// eslint-disable-next-line no-unused-vars
+const muscleGroups = {
+  back: 'Skip',
+  bicep: 'Skip',
+  calve: 'Skip',
+  chest: 'Skip',
+  frontDelt: 'Skip',
+  glute: 'Skip',
+  hamstring: 'Skip',
+  quad: 'Skip',
+  rearSideDelt: 'Skip',
+  tricep: 'Skip',
+  trap: 'Skip',
+};
+
+// export default function WorkoutGenerator() {
+//   const [muscles, setMuscles] = useState(muscleGroups);
+
+//   return (
+//     <>
+//       <div />
+//     </>
+//   );
+// }
